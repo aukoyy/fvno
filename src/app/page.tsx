da878cbbeb224166e72f5c7bcb9b598ddc93b10f
Øyvind Aukner <22494963+aukoyy@users.noreply.github.com>
2025-10-23 20:05:19 +0200

    hero image and text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,24 +4,31 @@ export default function Home() {
   return (
     <div className="">
       <main className="">
-        <div className="absolute md:ml-8 mt-100 md:mt-130 z-10 text-white  p-6 rounded-lg"> {/* bg-black/20 */}
+        <div className="absolute md:ml-8 mt-100 md:mt-130 z-10 text-white p-6 rounded-lg"> {/* bg-black/30 */}
           <h1 className="text-xl">VELKOMMEN TIL FULL VASE</h1>     
           <p className="mt-4 text-5xl">Frodig blosterdesign til ditt event</p>   
           <p className="mt-8 max-w-xl">Vi tror på blomster som forteller historier, om kjærlighet, glede og alt det vakre som gjør en dag uforglemmelig. Fyll vasen med minner og blomster.</p>
           <p className="mt-4 text-lg pl-8">-- Utforsk våre tjenester</p>
         </div>
         <Image
-          src="/hero-placeholder.jpg"
+          src="/fv-hero.jpg"
           alt="Full Vase Hero"
           width={1920}
           height={1080}
           className="w-screen h-screen object-cover"
         />
-        <div className="py-4 bg-fv-cyan-600">
-          <p>KLAR FOR EN FRODIG FULL VASE?</p>
+        <div className="py-4 bg-fv-cyan-600 overflow-hidden">
+          <div className="animate-scroll whitespace-nowrap">
+            {Array.from({ length: 100 }, (_, i) => (
+              <span key={i} className="text-white text-2xl font-medium tracking-wider mx-4">
+                KLAR FOR EN FRODIG FULL VASE?
+              </span>
+            ))}
+          </div>
         </div>
-        <div className="py-18 bg-fv-cyan-400">
-          <p className="text-7xl mx-24 text-center">Blomster du faktisk synes er skikkelig vakre. Vi designer din visjon til ditt event eller bryllup.</p>
+        <div className="py-18 bg-fv-cyan-400 flex flex-col items-center">
+          <p className="text-2xl md:text-6xl font-light text-center mx-18 max-w-2xl">Blomster du <i>faktisk</i> synes er skikkelig vakre. Vi designer din visjon til ditt event eller bryllup.</p>
+          <p className="mt-8 underline">Se tidligere arbeid</p>
         </div>
       </main>
     </div>
