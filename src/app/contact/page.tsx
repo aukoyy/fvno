c8a972da0b1649c9b5365fe9844ae3e496f811b7
Øyvind Aukner <22494963+aukoyy@users.noreply.github.com>
2025-11-06 20:57:58 +0100

    create contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,11 +1,17 @@
 export default function Contact() {
   return (
-    <main className="py-24 border-8 border-fuchsia-300 bg-emerald-300">
-      <div className="mt-12">
-        <h1>Contact Us</h1>
-        <p>If you have any questions, feel free to reach out!</p>
-
+    <main className="py-34">
+      <div className="text-center mt-16 max-w-lg mx-auto">
+        <h1 className="text-7xl">La oss snakke blomster!</h1>
+        <p className="mt-12 text-lg">Enten planene er spikret, eller bare i startfasen, gleder vi oss til å høre mer.</p>
+        <p className="mt-12 text-lg">Del gjerne litt om anledningen du ønsker blomster til nedenfor, så tar vi kontakt for å snakke om tilgjengelighet, uttrykk og neste steg.</p>
       </div>
+      <div className="py-30 bg-fv-300 text-center"><p>SKJEMA</p></div>
+      <div className="max-w-lg mx-auto mt-12">
+        <p>Hold et øye med innboksen, vi tar kontakt snart!</p>
+        <p>Vi pleier å svare innen 24 timer (mandag-fredag). Hører du ikke fra oss innen den tid, ta gjerne en titt i søppelpostmappen, bare for sikkerhets skyld.</p>
+        <p>Vi gleder oss til å høre fra deg!</p>
+      </div>  
     </main>
   );
 }
