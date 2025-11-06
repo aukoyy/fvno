ca16f1d95fa56f497d95f5c21219a1aa2528c7b2
Øyvind Aukner <22494963+aukoyy@users.noreply.github.com>
2025-11-06 20:45:20 +0100

    save

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,8 +1,11 @@
 export default function Contact() {
   return (
-    <div>
-      <h1>Contact Us</h1>
-      <p>If you have any questions, feel free to reach out!</p>
-    </div>
+    <main className="py-24 border-8 border-fuchsia-300 bg-emerald-300">
+      <div className="mt-12">
+        <h1>Contact Us</h1>
+        <p>If you have any questions, feel free to reach out!</p>
+
+      </div>
+    </main>
   );
 }
