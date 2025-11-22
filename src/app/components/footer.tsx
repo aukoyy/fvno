934dfa551903208902ae38e443316087b99e3b4b
Øyvind Aukner <22494963+aukoyy@users.noreply.github.com>
2025-11-22 14:50:29 +0100

    use link

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,15 +1,18 @@
 import { Button } from "antd";
+import Link from "next/link";
 
 export default function Footer() {
   return (
     <footer className="bg-fv-250 flex justify-center pb-24">
       <div className="px-12 py-24 max-w-8xl">
-        <p className="text-3xl md:text-6xl font-serif">La din blomsterdekorasjon være nestemanns inspirasjon.</p> {/* TODO: fix space bug. Triggers on screen size change */}
+        <p className="text-3xl md:text-6xl font-serif">La din blomsterdekorasjon være nestemanns inspirasjon.</p>
         <div className="mt-12 md:flex justify-between text-xl">
           <p>Hvilken anledning trenger du blomster til? </p>
-          <Button type="primary" href="/contact" size="large" shape="round" className="mt-6 md:mt-0">
-            Snakk med oss
-          </Button>
+          <Link href="/contact">
+            <Button type="primary" size="large" shape="round" className="mt-6 md:mt-0">
+              Snakk med oss
+            </Button>
+          </Link>
         </div>
         <hr className="text-fv-500 mt-12 max-w-sm" />
       </div>
