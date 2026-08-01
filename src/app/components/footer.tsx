adcee771d196ff4a72c75839defe54fcb0eab6c8
Øyvind Aukner <22494963+aukoyy@users.noreply.github.com>
2026-08-01 12:43:45 +0200

    Add icons

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,5 +1,7 @@
 import { Button } from "antd";
 import Link from "next/link";
+import { ExternalLinkIcon } from "./icons";
+import Image from "next/image";
 
 export default function Footer() {
   return (
@@ -16,7 +18,35 @@ export default function Footer() {
             </Button>
           </Link>
         </div>
-        <hr className="text-fv-500 mt-12 max-w-sm" />
+        <hr className="text-fv-500 mt-12" />
+        <div className="mt-12">
+          <p className="text-xl">MØT OSS ETTER AVTALE</p>
+          <a className="flex mt-4" href="https://maps.app.goo.gl/qidCEse4SE7P7wnT6" target="_blank" rel="noopener noreferrer">
+            <p>Grev Wedels Plass 2, 0151 Oslo</p>
+            <ExternalLinkIcon className="ml-2 w-4" />
+          </a>
+        </div>
+        <div className="mt-12 flex justify-end">
+          <p className="text-xl">Følg oss på</p>
+          <a href='https://www.instagram.com/fullvase.no/' target='_blank' rel='noopener noreferrer'>
+            <Image
+              src="/footer/instagram-icon.png" 
+              alt="Instagram" 
+              width={24}  
+              height={24} 
+              className="ml-4" 
+            />
+          </a>
+          <a href='https://www.facebook.com/profile.php?id=61565082137564' target='_blank' rel='noopener noreferrer'>
+            <Image 
+              src="/footer/facebook-icon.png" 
+              alt="Facebook" 
+              width={24}  
+              height={24} 
+              className="ml-4" 
+            />
+          </a>
+        </div>
       </div>
     </footer>
   );
